cd11ce7e89129ae58fdce55429f7e34e7c3ad917
Mauro Di Biasi <80405879+maurielloelollo@users.noreply.github.com>
2021-10-01 10:51:09 +0100

    sample commit

---
+++ wgetScript2.bat
@@ -0,0 +1,5 @@
+echo started
+wget -m -p -E -k -K -np "https://github.com/facebook/fbthrift/blob/master/thrift/compiler/test/fixtures/optionals/gen-java/Person.java" 
+wget -m -p -E -k -K -np "https://github.com/Sellegit/j2objc/blob/master/guava/sources/com/google/common/net/HttpHeaders.java" 
+wget -m -p -E -k -K -np "https://github.com/facebook/fbthrift/blob/main/thrift/compiler/test/fixtures/optionals/gen-java/Person.java" 
+wget -m -p -E -k -K -np "https://github.com/Sellegit/j2objc/blob/main/guava/sources/com/google/common/net/HttpHeaders.java" 
